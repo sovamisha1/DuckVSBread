814ad173477b01bf467ccd00b7317a117c7f4460
Mixail <mixail.potapov00@inbox.ru>
2022-09-21 21:16:18 +0300

    Теперь утка поворачивается при движении право/лево. Камера следует за игроком (костыль, надо фиксить, но пока ладно).

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -21,8 +21,30 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //обращаемся к компоненту персонажа RigidBody2D. задаем ему скорость по оси Х, 
+        //равную значению оси Х умноженное на значение макс. скорости
         float move = Input.GetAxis("Horizontal");
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        //если нажали клавишу для перемещения вправо, а персонаж направлен влево
+        if(move < 0 && !isRight)
+        //отражаем персонажа вправо
+            Flip();
+        //обратная ситуация. отражаем персонажа влево
+        else if (move > 0 && isRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        //меняем направление движения персонажа
+        isRight = !isRight;
+        //получаем размеры персонажа
+        Vector3 theScale = transform.localScale;
+        //зеркально отражаем персонажа по оси Х
+        theScale.x *= -1;
+        //задаем новый размер персонажа, равный старому, но зеркально отраженный
+        transform.localScale = theScale;
     }
 }
     
