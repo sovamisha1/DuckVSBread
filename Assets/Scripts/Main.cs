f08d131a7f7d82ade5a34a850e7815518db80fd7
Mixail <mixail.potapov00@inbox.ru>
2022-09-21 18:06:55 +0300

    Создал проект, есть скрипт движения

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    public float maxSpeed = 1f;
+    public bool rotate = true;
+    public float jump = 300f;
+    private bool _isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
+    
