0c600c5ce9b44f2331f6f88e28959381668ded9a
Mixail <mixail.potapov00@inbox.ru>
2022-09-23 21:25:34 +0300

    Добавил возможность прыжка + проверка от прыжка в воздухе

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,50 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
 
-    public float maxSpeed = 1f;
+    public float maxSpeed = 10f;
     public float jump = 300f;
     
     public bool isRight = false;
-    private bool _isGrounded;
+    private bool isGrounded;
+
+    public Transform GroundCheck;
+    public float checkRadius;
+    public LayerMask Ground;
+
+    
 
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        //обращаемся к компоненту персонажа RigidBody2D. задаем ему скорость по оси Х, 
-        //равную значению оси Х умноженное на значение макс. скорости
+        Jump();
+        Move();
+        GroundChecks();
+    }
+
+    private void Move()
+    {
         float move = Input.GetAxis("Horizontal");
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-        //если нажали клавишу для перемещения вправо, а персонаж направлен влево
-        if(move < 0 && !isRight)
-        //отражаем персонажа вправо
+        if(move > 0 && !isRight)
             Flip();
-        //обратная ситуация. отражаем персонажа влево
-        else if (move > 0 && isRight)
+        else if (move < 0 && isRight)
             Flip();
     }
-
     private void Flip()
     {
-        //меняем направление движения персонажа
         isRight = !isRight;
-        //получаем размеры персонажа
         Vector3 theScale = transform.localScale;
-        //зеркально отражаем персонажа по оси Х
         theScale.x *= -1;
-        //задаем новый размер персонажа, равный старому, но зеркально отраженный
         transform.localScale = theScale;
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump);
+        }
+    }
+
+    private void GroundChecks()
+    {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, Ground);
     }
 }
     
