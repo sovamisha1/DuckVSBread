db7e9cac2acb14053ad26bb97fc16d599fae0864
Mixail <mixail.potapov00@inbox.ru>
2022-09-23 21:44:10 +0300

    Добавил коментариев для понимания кода и немного его сократил

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,19 +5,6 @@ public class DeathZone : MonoBehaviour
 {
 
     public int respawn;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,39 +1,41 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
-
+    // переменные для движения
     public float maxSpeed = 10f;
     public float jump = 300f;
-    
     public bool isRight = false;
-    private bool isGrounded;
 
+    // переменные для предотвращения прыжка в воздухе
+    private bool isGrounded;
     public Transform GroundCheck;
-    public float checkRadius;
+    public float checkRadius = 0.2f;
     public LayerMask Ground;
 
-    
+    // перемнная для хранения в ней RigidBody
+    Rigidbody2D rb;
 
-
-    void Start()
+    private void Start()
     {
-        
+        // переменная rb берет компонент RigidBody для упрощения кода
+        rb = GetComponent<Rigidbody2D>();
     }
-
     void Update()
     {
         Jump();
         Move();
         GroundChecks();
     }
-
+    
     private void Move()
     {
+        // переменная move отвечает за a,d right arrow, left arrow
+        // изменение позиции перса
         float move = Input.GetAxis("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
 
+        // проверка для отображения поворота персонажа
         if(move > 0 && !isRight)
             Flip();
         else if (move < 0 && isRight)
@@ -51,12 +53,13 @@ public class Main : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump);
+            rb.AddForce(Vector2.up * jump);
         }
     }
 
     private void GroundChecks()
     {
+        // присваиваем isGrounded True если GroundCheck.position с радиусом checkRadius касается слоя Ground
         isGrounded = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, Ground);
     }
 }
